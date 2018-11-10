441a43ab208ddd338bf135c7e7ed67ccd989da84
Justin <justinzhongvs@gmail.com>
2018-11-09 16:34:41 -0800

    add questions

--- src/components/quiz/index.js
+++ src/components/quiz/index.js
@@ -5,14 +5,27 @@ import './Quiz.css';
 
 class Quiz extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			score: 0
+		};
+	}
+
+	update_score(prev, next) {
+		this.setState({
+			score: this.state.score - parseInt(prev) + parseInt(next)
+		});
+	}
+
 	get_questions() {
 		var q = [];
-		console.log(questions);
 		questions.questions.forEach((qu, i) => {
-    	console.log(qu);
 			q.push(
 				<QuizQuestion
 					question={qu.question}
+					options={qu.options}
+					update_score={(x,y) => this.update_score(x,y)}
 					key={qu.id}
 				/>
 			);
@@ -33,13 +46,62 @@ class Quiz extends Component {
 }
 
 class QuizQuestion extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			answered: false,
+			selected: null,
+			score: 0
+		}
+	}
+
+	handle_click(i) {
+		let old = (!this.state.selected) ? 0 : this.state.selected;
+		this.props.update_score(old, i);
+		this.setState({
+			answered: true,
+			selected: i
+		});
+	}
+	
+	get_rows() {
+		var rows = [];
+		if (!this.props || !this.props.options) {
+			return;
+		}
+		Object.entries(this.props.options).forEach((k, v) => {
+			let selected = "";
+			if (this.state.selected === k[0]) {
+				selected = "selected"	
+			}
+			rows.push(
+				<tr key={k[1]} className={selected} onClick={() => this.handle_click(k[0])}>
+					<td>{k[1]}</td>
+				</tr>
+			)
+		});
+		return rows;
+	}
+
+	get_indicator() {
+		return (!this.state.answered) ?
+			(<i className="far fa-circle indicator"></i>) :
+			(<i className="fas fa-check-circle indicator"></i>);
+	}
+
 	render () {
+		const rows = this.get_rows();
 		return (
 			<div className="quiz-question-container"> 
+				{ this.get_indicator() }
 				<h2 className="quiz-question">
 					{ this.props.question }
 				</h2>
 				<table> 
+					<tbody>
+						{ rows }	
+					</tbody>
 				</table>
 			</div>
 		);
