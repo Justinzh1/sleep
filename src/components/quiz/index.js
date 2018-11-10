656110b95aba931deb7d3ba85c0d7dc31fc9a9ee
Justin <justinzhongvs@gmail.com>
2018-11-09 16:41:41 -0800

    finished questions

--- src/components/quiz/index.js
+++ src/components/quiz/index.js
@@ -40,6 +40,11 @@ class Quiz extends Component {
 				<h2 className="quiz-question">Are you a morning person or an evening person?</h2>
 				<p>The morningness-eveningness test is a way to measure when you are the most productive. This test features a systematic way to evaluate your score. After filling out the following sections you can see the results along with how you compare with others. </p>
 				{q}
+				<br />
+				<div className="quiz-results">
+					<h2 className="quiz-question">Your Morningness-Eveningness Score is {this.state.score} </h2>
+					<p> Higher scores mean that you are more of a morning person, whereas lower scores mean that you are more of an evening person. The score can range from 23 to 76.</p>
+				</div>
 			</div>
 		);
 	}
