390190d71f175c7f1b919f22a5fe0302b8de66b1
Daniel Mao <daniel.mao@berkeley.edu>
2018-11-25 00:53:39 -0800

    added faq answers

--- src/components/faq.js
+++ src/components/faq.js
@@ -3,77 +3,124 @@ export default {
 		{ 
 				question: "Why do we have 'afternoon slump' usually always at the same time?",
 				asked: 1,
-				answer: "idk"
+				answer: "To understand this phenomena, we first need to understand the two process model \
+				of sleep-wake regulation. There are two factors that contribute to this sleep-wake rhythm: \
+				the circadian factor, or process-c and the amount of time elapsed since a person woke up, \
+				process-s, the drive to sleep. As the circadian rhythm (process-c) increases, our drive to be \
+				awake increases and vice-versa. Whereas the longer awake, the more pressure there is too sleep, \
+				controlled by the chemical adenosine. Thus, the greatest urge to sleep occurs when there is the \
+				largest distance between process-s and process-c. Generally, this occurs around the afternoon."
 		},
 		{ 
 				question: "Why do some people have insomnia??",
 				asked: 1,
-				answer: "idk"
+				answer: "Insomnia can be caused by a variety of psychiatric and medical conditions, \
+				unhealthy sleep habits, substances, and/or biological factors. Recent studies have \
+				begun to make scientists believe that insomnia is a result of your brain being unable \
+				to stop being awake"
 		},
-		{ 
+		{ 		
 				question: "What is the recommended horus of sleep per age group?",
 				asked: 1,
-				answer: "idk"
+				answer: "Newborns (0 - 3 months): 14 - 17 hours per day\n \
+				Infants (4 - 11 months): 12 - 15 hours per day\n \
+				Toddlers (1 - 2 years): 11 - 14 hours per day \n \
+				Preschoolers (3 - 5 years): 10 - 13 hours per day\n \
+				School age (6 - 13 years): 9 - 11 hours per day\n \
+				Teenagers (14 - 17 years): 8 - 10 hours per day\n \
+				Young adults (18 - 25 years): 7 - 9 hours per day\n \
+				Adults (26 - 64 years): 7 - 9 hours per day\n \
+				Older adults (65+ years): 7 - 8 hours per day"
 		},
 		{ 
 				question: "Is it true that the number of hours you sleep should be a multiple of 90 mintues?",
 				asked: 1,
-				answer: "idk"
+				answer: "The amount of time you should sleep is however much time you need to feel well rested. \
+				There is so much variability in the duration of sleep cycles between individuals that it is \
+				impossible to generalize a ‘perfect amount of sleep’ and certainly not one that is so specific \
+				such as 90 minutes."
 		},
 		{ 
 				question: "Are biphasic sleep schedules unhealthy?",
 				asked: 1,
-				answer: "idk"
+				answer: "Evidence to the contrary has shown that a biphasic sleep cycle, long sleep at night and \
+				short nap during the afternoon, might be a possibility for how people should sleep as observed in \
+				pre-industrial hunter gatherer tribes. This isn’t unreasonable as humans are genetically hardwired \
+				to have a dip in alertness around the afternoon."
 		},
 		{ 
 				question: "Is sleep cumulative?",
 				asked: 1,
-				answer: "idk"
+				answer: "sleep is not cumulative in the respect that we can make up all the sleep we may lose pulling \
+				all-nighters or any lack of sleep for several hours. You may not recover all the sleep you lose and \
+				remain in a sleep deficit. However, even if a loss of sleep happens, the body has its own recovery \
+				system that makes it easier to bounce back from."
 		},
 		{ 
 				question: "Is it necessarily healthier to sleep and wake up earlier?",
 				asked: 1,
-				answer: "idk"
+				answer: "Not necessarily. It depends on the person. As long as you receive an adequate amount of quality \
+				sleep for the recommended number of hours needed for your age group, sleeping should be personalized to the \
+				individual"
 		},
 		{ 
 				question: "Is it healthy to pull all nights?",
 				asked: 1,
-				answer: "No"
+				answer: "Absolutely not. There are several dangers of sleep deprivation. Studies have shown that if you have been \
+				awake for more than +20 hours you are as impaired as a drunk driver (BAC 0.1%), Sleep deprivation can lead to mental \
+				health problems, psychosis and possibly death. The World Health Organization recognizes as sleep deprivation as a \
+				carcinogen linked to cancer development death."
 		},
 		{ 
 				question: "Trying to sleep earlier had never worked for me, what are some ways to fall asleep faster?",
 				asked: 1,
-				answer: "idk"
+				answer: "some tips to fall asleep earlier include listening to soothing sounds at night when attempting \
+				to sleep, expending more energy than usual during the day, cut out caffeine, alcohol, adjusting the \
+				temperature to be cooler but not cold, etc."
 		},
 		{ 
 				question: "Why do we dream?",
 				asked: 1,
-				answer: "idk"
+				answer: "there are several theories as to why we dream. Here are a few to consider. First, one theory \
+				suggests we dream to unlearn. Throughout the day we gather a vast amount of information, some unnecessary \
+				that we flush from our brains by dreaming. Another theory suggests the possibility that we dream for emotional \
+				therapy. By dreaming we dissolve the emotional trauma of certain events we encounter. A correlation has been \
+				found between sufferers of PTSD and their ability, or lack thereof, of dreaming."
 		},
 		{ 
 				question: "How does alcohol affect your sleep?",
 				asked: 1,
-				answer: "idk"
+				answer: "In the body, alcohol disrupts circadian functioning, directly interfering with the ability of the part \
+				of the brain region that controls our circadian rhythm to synchronize itself. Thus, alcohol may trigger new sleep \
+				disorders or exacerbate existing ones, including insomnia and sleep apnea. Studies also show that alcohol affects \
+				the typical structure of sleep which is not healthy for individuals."
 		},
 		{ 
 				question: "Why do we need to sleep?",
 				asked: 1,
-				answer: "idk"
+				answer: "Our bodies need sleep in order to grow muscle, repair tissue, synthesize hormones, retain information, deal \
+				with traumatic experiences, etc. Sleep is essential for all living organisms."
 		},
 		{ 
 				question: "What techniques are proven to help people sleep faster?",
 				asked: 1,
-				answer: "idk"
+				answer: "proven techniques to help people sleep better include lowering the room temperature to a comfortable cool \
+				temperature, following a sleep schedule, avoiding naps during the day, and expending sufficient energy during the \
+				day to name a few"
 		},
 		{ 
 				question: "How can I get higher quality sleep?",
 				asked: 1,
-				answer: "idk"
+				answer: "try going to sleep at the same time every day, avoid sleeping in, avoid napping, exercise daily, evaluate \
+				your room - adjust to a cool temperature, be free from noise, be free from light, sleep on comfortable mattress and \
+				pillows"
 		},
 		{ 
 				question: "Why is it so ahrd to maintain a good sleep schedule?",
 				asked: 1,
-				answer: "idk"
+				answer: "there are several factors that play a part in affecting an individual’s personal sleep schedule. Social aspects \
+				as well as professional aspects can affect a person’s sleep schedule. The key is to try to keep a consistent schedule \
+				and not stay up later than what your own personal schedule demands."
 		}
 	]
 }
